7d631ed94cf760f7e1cd96d38f755a51f04011af
Jayanth <jayanthedam@gmail.com>
2024-09-14 16:43:36 +0530

    Jayanth001-created Write a program to check if the num is Even and odd

---
+++ src/main/java/com/example/javamavenjunithelloworld/Utility.js
@@ -0,0 +1,11 @@
+function Jayanth(){
+    console.log("Im Jayanth");
+}
+function EveOrOdd(num){
+    if(num%2 == 0){
+        console.log("Num is Even");
+    }
+    else{
+        console.log("Num is Odd");
+    }
+}
